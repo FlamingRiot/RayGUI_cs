31d6f41c9486c3e9a909d3327181a119d123ff1f
= <=>
2024-02-05 10:11:01 +0100

    Setup : mise en place du gitignore

---
+++ Button.cs
@@ -0,0 +1,52 @@
+﻿using Raylib_cs;
+namespace RayGUI_cs
+{
+    /// <summary>
+    /// 2D button
+    /// </summary>
+    public partial struct Button
+    {
+        /// <summary>
+        /// Button width
+        /// </summary>
+        public int Width;
+
+        /// <summary>
+        /// Button height
+        /// </summary>
+        public int Height;
+
+        /// <summary>
+        /// Background color for the button
+        /// </summary>
+        public Color Color;
+
+        /// <summary>
+        /// Border color for the button
+        /// </summary>
+        public Color BorderColor;
+
+        /// <summary>
+        /// Hover color for the button
+        /// </summary>
+        public Color HoverColor;
+
+        public Button(int width, int height, Color color, Color borderColor)
+        {
+            Width = width;
+            Height = height;
+            Color = color;
+            BorderColor = borderColor;
+            HoverColor = color;
+        }
+
+        public Button(int width, int height, Color color, Color borderColor, Color hoverColor)
+        {
+            Width = width;
+            Height = height;
+            Color = color;
+            BorderColor = borderColor;
+            HoverColor = hoverColor;
+        }
+    }
+}
---
+++ RayGUI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayGUI_cs
+{
+    public static unsafe partial class RayGUI
+    {
+        public static void DrawButton()
+        {
+
+        }
+    }
+}
