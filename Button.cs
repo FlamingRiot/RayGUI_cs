8442df548d3d4dfdb3c33087f198cb4ccb45434b
Evan Comtesse <ComtesseE1@s2.rpn.ch>
2024-02-05 12:40:28 +0100

    Implementation : Affichage de boutons et vérification d'interaction

--- Button.cs
+++ Button.cs
@@ -31,22 +31,47 @@ namespace RayGUI_cs
         /// </summary>
         public Color HoverColor;
 
-        public Button(int width, int height, Color color, Color borderColor)
+        /// <summary>
+        /// X coordinate of the button
+        /// </summary>
+        public int X;
+
+        /// <summary>
+        /// Y coordinate of the button
+        /// </summary>
+        public int Y;
+
+        /// <summary>
+        /// Text of the button
+        /// </summary>
+        public string Text;
+
+        public Button(int width, int height, int x, int y, Color color, Color borderColor)
         {
             Width = width;
             Height = height;
+            X = x;
+            Y = y;
             Color = color;
             BorderColor = borderColor;
             HoverColor = color;
+
+            // Automatically set
+            Text = "";
         }
 
-        public Button(int width, int height, Color color, Color borderColor, Color hoverColor)
+        public Button(int width, int height, int x, int y, Color color, Color borderColor, Color hoverColor)
         {
             Width = width;
             Height = height;
+            X = x;
+            Y = y;
             Color = color;
             BorderColor = borderColor;
             HoverColor = hoverColor;
+
+            // Automatically set
+            Text = "";
         }
     }
 }
--- RayGUI.cs
+++ RayGUI.cs
@@ -1,16 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using static Raylib_cs.Raylib;
+using Raylib_cs;
+using System.Numerics;
 
 namespace RayGUI_cs
 {
     public static unsafe partial class RayGUI
     {
-        public static void DrawButton()
-        {
+        const int BORDER = 2;
 
+        //------------------------------------------------------------------------------------
+        // Window and Graphics Device Functions (Module: core)
+        //------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Draw button on the screen
+        /// </summary>
+        /// <param name="button">Button to draw</param>
+        /// <param name="position">Positon to draw the button</param>
+        public static void DrawButton(Button button, Font font)
+        {
+            DrawRectangle((int)button.X - BORDER, (int)button.Y - BORDER, button.Width + BORDER * 2, button.Height + BORDER * 2, button.BorderColor);
+            // Manage hover button color
+            Vector2 mouse = GetMousePosition();
+            if (mouse.X < button.X + button.Width && mouse.X > button.X && mouse.Y < button.Y + button.Height && mouse.Y > button.Y) 
+            {
+                DrawRectangle((int)button.X, (int)button.Y, button.Width, button.Height, button.HoverColor);
+            }
+            else
+            {
+                DrawRectangle((int)button.X, (int)button.Y, button.Width, button.Height, button.Color);
+            }
+            // Manage button text
+            font.BaseSize = 2;
+            int txtLength = button.Text.Length;
+            DrawTextPro(font, button.Text, new Vector2(button.X + button.Width / 2 - txtLength * 4, button.Y + button.Height / 3 -  5), new Vector2(0, 0), 0, 1, 1, Color.White);
+        }
+
+        /// <summary>
+        /// Check if a button is pressed, returns boolean
+        /// </summary>
+        /// <param name="button">Button to check</param>
+        /// <returns></returns>
+        public static bool IsButtonPressed(Button button)
+        {
+            Vector2 mouse = GetMousePosition();
+            if (mouse.X < button.X + button.Width && mouse.X > button.X && mouse.Y < button.Y + button.Height && mouse.Y > button.Y && IsMouseButtonPressed(MouseButton.Left))
+            {
+                    return true;
+            }
+            else { return false; }
         }
     }
 }
