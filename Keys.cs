1005b4c9fd18e6233bcbeb9a7c714d31c3606201
= <=>
2024-02-22 11:16:21 +0100

    Implementation : struct des clés de clavier pour convertir les keycodes

---
+++ Keys.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayGUI_cs
+{
+    public partial struct Keys
+    {
+        public string[] Key;
+
+        /// <summary>
+        /// Define keys
+        /// </summary>
+        public Keys()
+        {
+            Key = new string[]
+            {
+                "a",
+                "b",
+                "c",
+                "d",
+                "e",
+                "f",
+                "g",
+                "h",
+                "i",
+                "k",
+                "l",
+                "m",
+                "n",
+                "o",
+                "p",
+                "q",
+                "r",
+                "s",
+                "t",
+                "u",
+                "v",
+                "w",
+                "x",
+                "y",
+                "z"
+            };
+        }
+    }
+}
--- RayGUI.cs
+++ RayGUI.cs
@@ -7,14 +7,8 @@ namespace RayGUI_cs
 {
     public unsafe partial class RayGUI
     {   
-        // Load data from external files
-
         const int BORDER = 1;
-        static StreamReader sr = new StreamReader("data/keycodes.json");
-        static string json = sr.ReadToEnd();
-        static readonly List<string> KEYCODE = JsonConvert.DeserializeObject<List<string>>(json);
-
-
+        static Keys KEYS = new Keys();
         //------------------------------------------------------------------------------------
         // Window and Graphics Device Functions (Module: core)
         //------------------------------------------------------------------------------------
@@ -302,7 +296,7 @@ namespace RayGUI_cs
         /// <returns></returns>
         public static string GetKeyString(int keycode)
         {
-            return externalData;
+            return KEYS.Key[keycode - 67];
         }
     }
 }
