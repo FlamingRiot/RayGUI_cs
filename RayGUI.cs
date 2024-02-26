42cf5ee2f7f0f2f6a6c0bbffb0155ae14056f4db
Evan Comtesse <ComtesseE1@s2.rpn.ch>
2024-02-26 15:17:39 +0100

    Implementation : curseurs spécifiques

--- RayGUI.cs
+++ RayGUI.cs
@@ -35,8 +35,9 @@ namespace RayGUI_cs
             DrawRectangle(button.X - BORDER, button.Y - BORDER, button.Width + BORDER * 2, button.Height + BORDER * 2, button.BorderColor);
             // Manage hover button color
             Vector2 mouse = GetMousePosition();
-            if (mouse.X < button.X + button.Width && mouse.X > button.X && mouse.Y < button.Y + button.Height && mouse.Y > button.Y) 
+            if (Hover(button.X, button.Y, button.Width, button.Height)) 
             {
+                SetMouseCursor(MouseCursor.PointingHand);
                 DrawRectangle((int)button.X, (int)button.Y, button.Width, button.Height, button.HoverColor);
             }
             else
@@ -59,8 +60,7 @@ namespace RayGUI_cs
         /// <returns></returns>
         public static bool IsButtonPressed(Button button)
         {
-            Vector2 mouse = GetMousePosition();
-            if (mouse.X < button.X + button.Width && mouse.X > button.X && mouse.Y < button.Y + button.Height && mouse.Y > button.Y && IsMouseButtonPressed(MouseButton.Left))
+            if (Hover(button.X, button.Y, button.Width, button.Height) && IsMouseButtonPressed(MouseButton.Left))
             {
                 button.Activate();
                 return true;
@@ -74,95 +74,10 @@ namespace RayGUI_cs
         /// <param name="c"></param>
         public static void DrawContainer(ref Container c)
         {
-            // Manage the resize option
-            Vector2 mouse = GetMousePosition();
-            switch (mouse)
-            {
-                // Left
-                case Vector2 i when i.X < c.X + 5 && i.X > c.X - 5 && i.Y < c.Height - 5 && i.Y > c.Y + 5:
-                    SetMouseCursor(MouseCursor.ResizeEw);
-                    if (IsMouseButtonDown(MouseButton.Left)) { c.Resize = "left"; }
-                    break;
-                // Right
-                case Vector2 i when i.X < c.X + c.Width + 5 && i.X > c.X + c.Width - 5 && i.Y < c.Height - 5 && i.Y > c.Y + 5:
-                    SetMouseCursor(MouseCursor.ResizeEw);
-                    if (IsMouseButtonDown(MouseButton.Left)) { c.Resize = "right"; }
-                    break;
-                // Top
-                case Vector2 i when i.X < c.X + c.Width - 5 && i.X > c.X + 5 && i.Y < c.Y + 5 && i.Y > c.Y - 5:
-                    SetMouseCursor(MouseCursor.ResizeNs);
-                    if (IsMouseButtonDown(MouseButton.Left)) { c.Resize = "top"; }
-                    break;
-                // Bottom
-                case Vector2 i when i.X < c.X + c.Width - 5 && i.X > c.X + 5 && i.Y < c.Y + c.Height + 5 && i.Y > c.Y + c.Height - 5:
-                    SetMouseCursor(MouseCursor.ResizeNs);
-                    if (IsMouseButtonDown(MouseButton.Left)) { c.Resize = "bottom"; }
-                    break;
-                // Top-Left
-                case Vector2 i when i.X < c.X + 5 && i.X > c.X - 5 && i.Y < c.Y + 5 && i.Y > c.Y - 5:
-                    SetMouseCursor(MouseCursor.ResizeNwse);
-                    if (IsMouseButtonDown(MouseButton.Left)) { c.Resize = "top-left"; }
-                    break;
-                // Top-Right
-                case Vector2 i when i.X < c.X + c.Width + 5 && i.X > c.X + c.Width - 5 && i.Y < c.Y + 5 && i.Y > c.Y - 5:
-                    SetMouseCursor(MouseCursor.ResizeNesw);
-                    if (IsMouseButtonDown(MouseButton.Left)) { c.Resize = "top-right"; }
-                    break;
-                // Bottom-Left
-                case Vector2 i when i.X < c.X + 5 && i.X > c.X - 5 && i.Y < c.Y + c.Height + 5 && i.Y > c.Y + c.Height - 5:
-                    SetMouseCursor(MouseCursor.ResizeNesw);
-                    if (IsMouseButtonDown(MouseButton.Left)) { c.Resize = "bottom-left"; }
-                    break;
-                // Bottom-Right
-                case Vector2 i when i.X < c.X + c.Width + 5 && i.X > c.X + c.Width - 5 && i.Y < c.Y + c.Height + 5 && i.Y > c.Y + c.Height - 5:
-                    SetMouseCursor(MouseCursor.ResizeNwse);
-                    if (IsMouseButtonDown(MouseButton.Left)) { c.Resize = "bottom-right"; }
-                    break;
-                // Not
-                default:
-                    SetMouseCursor(MouseCursor.Default);
-                    break;
-            }
-
-            // Stop the resize option
-            if (IsMouseButtonReleased(MouseButton.Left)) { c.Resize = "none"; }
-            
-            switch (c.Resize)
-            {
-                case "left":
-                    c.X = mouse.X;
-                    break;
-                case "right":
-                    c.Width = (int)mouse.X - (int)c.X;
-                    break;
-                case "top":
-                    c.Y = mouse.Y;
-                    break;
-                case "bottom":
-                    c.Height = (int)mouse.Y - (int)c.Y;
-                    break;
-                case "bottom-right":
-                    c.Width = (int)mouse.X - (int)c.X;
-                    c.Height = (int)mouse.Y - (int)c.Y;
-                    break;
-                case "bottom-left":
-                    c.Height = (int)mouse.Y - (int)c.Y;
-                    c.X = mouse.X;
-                    break;
-                case "top-left":
-                    c.Y = mouse.Y;
-                    c.X = mouse.X;
-                    break;
-                case "top-right":
-                    c.Y = mouse.Y;
-                    c.Width = (int)mouse.X - (int)c.X;
-                    break;
-            }
-
             // Manage FileDropper containers
             if (c.Type == ContainerType.FileDropper)
             {
-                if (IsFileDropped() && mouse.X < c.X + c.Width && mouse.X > c.X && mouse.Y < c.Y + c.Height && mouse.Y > c.Y)
+                if (IsFileDropped() && Hover((int)c.X, (int)c.Y, c.Width, c.Height))
                 {
                     ImportFiles(c);
                 }
@@ -207,10 +122,9 @@ namespace RayGUI_cs
         {
             int size = 16;
             int border = 1;
-            Vector2 mouse = GetMousePosition();
 
             // Manage ticking option
-            if (mouse.X < t.X + size && mouse.X > t.X && mouse.Y < t.Y + size && mouse.Y > t.Y)
+            if (Hover(t.X, t.Y, size, size))
             {
                 DrawRectangle(t.X - border, t.Y - border, size + border * 2, size + border * 2, t.BorderColor);
                 if (IsMouseButtonPressed(MouseButton.Left))
@@ -270,12 +184,13 @@ namespace RayGUI_cs
             if (Hover(t.X, t.Y, t.Width, t.Height))
             {
                 SetMouseCursor(MouseCursor.IBeam);
-                if (IsMouseButtonPressed(MouseButton.Left)) 
+                if (IsMouseButtonPressed(MouseButton.Left))
                 {
                     t.Focus = true;
                     t.Color = ColorTint(t.Color, Color.Blue);
                 }
             }
+            else { SetMouseCursor(MouseCursor.Default); }
             if (t.Focus)
             {
                 int key = GetKeyPressed();
