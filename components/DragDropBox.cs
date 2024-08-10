d30ea8f092edf054fc8601b8ffc131dbfe19704c
Evan Comtesse <ComtesseE1@s2.rpn.ch>
2024-08-10 11:49:58 +0200

    Écriture de la classe DragDropBox

---
+++ components/DragDropBox.cs
@@ -0,0 +1,64 @@
+﻿using Raylib_cs;
+namespace RayGUI_cs
+{
+    public class DragDropBox : Component
+    {
+        /// <summary>
+        /// Box text
+        /// </summary>
+        private string text;
+        /// <summary>
+        /// Box main color
+        /// </summary>
+        private Color color;
+        /// <summary>
+        /// Box secondary color
+        /// </summary>
+        private Color borderColor;
+        /// <summary>
+        /// Box text
+        /// </summary>
+        public string Text { get { return text; } set { text = value; } }
+        /// <summary>
+        /// Box main color
+        /// </summary>
+        public Color Color { get { return color; } set { color = value; } }
+        /// <summary>
+        /// Box secondary color
+        /// </summary>
+        public Color BorderColor { get { return borderColor; } set { borderColor = value; } }
+        /// <summary>
+        /// DragDropBox constructor
+        /// </summary>
+        /// <param name="x">Box X position</param>
+        /// <param name="y">Box Y postiton</param>
+        /// <param name="width">Box width</param>
+        /// <param name="height">Box height</param>
+        /// <param name="text">Box text</param>
+        /// <param name="color">Box main color</param>
+        /// <param name="borderColor">Box secondary color</param>
+        public DragDropBox(int x, int y, int width, int height, string text, Color color, Color borderColor) : base(x, y, width, height)
+        {
+            this.text = text;
+            this.color = color;
+            this.borderColor = borderColor;
+        }
+        /// <summary>
+        /// DragDropBox constructor
+        /// </summary>
+        /// <param name="x">Box X position</param>
+        /// <param name="y">Box Y postiton</param>
+        /// <param name="width">Box width</param>
+        /// <param name="height">Box height</param>
+        /// <param name="text">Box text</param>
+        /// <param name="color">Box main color</param>
+        /// <param name="borderColor">Box secondary color</param>
+        /// <param name="tag">Box tag</param>
+        public DragDropBox(int x, int y, int width, int height, string text, Color color, Color borderColor, string tag) : base(x, y, width, height, tag)
+        {
+            this.text = text;
+            this.color = color;
+            this.borderColor = borderColor;
+        }
+    }
+}
